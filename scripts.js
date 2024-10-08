04bbf79bd6233cbbec7200c0cd94b265a506bf09
Vitória Miranda <vitoriamirandaa5@gmail.com>
2024-10-08 11:40:21 -0300

    criando

---
+++ scripts.js
@@ -0,0 +1,64 @@
+class BoxShadowGenerator {
+
+    constructor(horizontal, horizontalRef, vertical,verticalRef, blur, blurRef, spread, spreadRef, previewBox, rule, webkitRule, mozRule) 
+    {
+        this.horizontal = horizontal; 
+        this.horizontalRef = horizontalRef;
+        this.vertical = vertical; 
+        this.verticalRef = verticalRef; 
+        this.blur = blur;
+        this.blurRef = blurRef;
+        this.spread = spread; 
+        this.spreadRef = spreadRef; 
+        this.previewBox = previewBox;
+        this.rule = rule; 
+        this.webkitRule = webkitRule; 
+        this.mozRule = mozRule; 
+    }
+
+    initialize() {
+        this.horizontalRef.value = this.horizontal.value;
+        this.verticalRef.value = this.vertical.value;
+        this.spreadRef.value = this.spread.value;
+        this.blurRef.value = this.blur.value;
+
+        this.applyRule();
+        this.showRule();
+    }
+
+    applyRule() {
+        this.previewBox.style.boxShadow = `${this.horizontalRef.value}px ${this.verticalRef.value}px ${this.blurRef.value}px ${this.spreadRef.value}px #000000`
+        this.currentRule = this.previewBox.style.boxShadow;
+    }
+
+    showRule() {
+        this.rule.innerText = this.currentRule;
+        this.webkitRule.innerText = this.currentRule;
+        this.mozRule.innerText = this.currentRule;
+    }
+
+}
+
+// Seleção de elementos
+const horizontal = document.querySelector("#horizontal");
+const horizontalRef = document.querySelector("#horizontal-value");
+const vertical = document.querySelector("#vertical");
+const verticalRef = document.querySelector("#vertical-value");
+const blur = document.querySelector("#blur");
+const blurRef = document.querySelector("#blur-value");
+const spread = document.querySelector("#spread");
+const spreadRef = document.querySelector("#spread-value");
+
+const previewBox = document.querySelector("#box");
+
+const rule = document.querySelector("#rule span");
+const webkitRule = document.querySelector("#webkit-rule");
+const mozRule = document.querySelector("#moz-rule");
+
+const boxShadow = new BoxShadowGenerator(
+    horizontal, horizontalRef, vertical,verticalRef, blur, blurRef, spread, spreadRef, previewBox, rule, webkitRule, mozRule
+);
+
+boxShadow.initialize();
+
+// Eventos
